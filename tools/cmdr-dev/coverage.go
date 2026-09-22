fdbb7916dda9059e08d02a099b778eabf6051b15
tobianahillel-afk <269188945+tobianahillel-afk@users.noreply.github.com>
2026-09-22 22:20:55 +0200

    style: apply gofmt to coverage counters

--- tools/cmdr-dev/coverage.go
+++ tools/cmdr-dev/coverage.go
@@ -39,18 +39,18 @@ type ProductGraph struct {
 }
 
 type CoverageGraphSummary struct {
-	Entities                      int    `json:"entities"`
-	Edges                         int    `json:"edges"`
-	OwnedCapabilities             int    `json:"owned_capabilities"`
-	ReferenceOnlyCapabilities     int    `json:"reference_only_capabilities"`
-	DistinctRequirementReferences int    `json:"distinct_requirement_references"`
-	DistinctOpenDecisionReferences int   `json:"distinct_open_decision_references"`
-	DistinctPermissionReferences  int    `json:"distinct_permission_references"`
-	OwnedScreens                  int    `json:"owned_screens"`
-	ReferenceOnlyScreens          int    `json:"reference_only_screens"`
-	SpecTreeDigest                string `json:"spec_tree_digest"`
-	Output                        string `json:"output"`
-	Mode                          string `json:"mode"`
+	Entities                       int    `json:"entities"`
+	Edges                          int    `json:"edges"`
+	OwnedCapabilities              int    `json:"owned_capabilities"`
+	ReferenceOnlyCapabilities      int    `json:"reference_only_capabilities"`
+	DistinctRequirementReferences  int    `json:"distinct_requirement_references"`
+	DistinctOpenDecisionReferences int    `json:"distinct_open_decision_references"`
+	DistinctPermissionReferences   int    `json:"distinct_permission_references"`
+	OwnedScreens                   int    `json:"owned_screens"`
+	ReferenceOnlyScreens           int    `json:"reference_only_screens"`
+	SpecTreeDigest                 string `json:"spec_tree_digest"`
+	Output                         string `json:"output"`
+	Mode                           string `json:"mode"`
 }
 
 var capabilityIDPattern = regexp.MustCompile(`^CAP-[A-Z0-9]+-[0-9]{3}$`)
